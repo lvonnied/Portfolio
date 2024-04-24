1fdb6b15bbac0ddf67e30ab9cd7e97a053e5bad0
Lucas <lucaskvn@gmail.com>
2024-04-24 16:44:15 +0200

    Replaced <a> tags with routerLink and imported routermodule

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatToolbarRow, MatToolbar } from '@angular/material/toolbar'
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatToolbarRow, MatToolbar],
+  imports: [MatToolbarRow, MatToolbar, RouterModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
